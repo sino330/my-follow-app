86ee6907d9bf8676de8ea759d7612273b13018ed
sino330 <narutokozo330@gmail.com>
2021-10-31 20:06:33 +0900

    2021/10/31

--- src/App.js
+++ src/App.js
@@ -76,3 +76,121 @@ function App() {
 }
 
 export default App;
+
+//Jquery
+// <!-- <!DOCTYPE html>
+// <html lang="ja">
+//   <head>
+//     <link rel="stylesheet" href="style.css" />
+//   </head>
+//   <body data-feedly-mini="yes">
+//     <main>
+//       <div id="app">
+//         <ul class="accountList">
+//           <li
+//             class="accountList__item js-account-item"
+//             data-follow="false"
+//             data-id="1"
+//           >
+//             <div>
+//               <div class="account__summary">
+//                 <div>
+//                   <p class="account__name">リオネル・メッシ</p>
+//                   <p class="account__team">FCバルセロナ</p>
+//                 </div>
+//                 <div>
+//                   <button type="button" class="js-follow-btn followBtn">
+//                     フォローする
+//                   </button>
+//                 </div>
+//               </div>
+//               <p class="account__description">
+//                 アルゼンチンサンタフェ州ロサリオ出身のイタリア系アルゼンチン人サッカー選手。リーガ・エスパニョーラ・FCバルセロナ所属。アルゼンチン代表。ポジションはフォワード
+//                 (wikipedia)
+//               </p>
+//             </div>
+//           </li>
+//           <li
+//             class="accountList__item js-account-item"
+//             data-follow="true"
+//             data-id="2"
+//           >
+//             <div>
+//               <div class="account__summary">
+//                 <div>
+//                   <p class="account__name">クリスティアーノ・ロナウド</p>
+//                   <p class="account__team">Juventus</p>
+//                 </div>
+//                 <div>
+//                   <button type="button" class="js-follow-btn followBtn">
+//                     フォロー中
+//                   </button>
+//                 </div>
+//               </div>
+//               <p class="account__description">
+//                 ポルトガル・フンシャル出身のサッカー選手。セリエA・ユヴェントスFC所属。ポルトガル代表。ポジションはフォワード
+//                 (wikipedia)
+//               </p>
+//             </div>
+//           </li>
+//           <li
+//             class="accountList__item js-account-item"
+//             data-follow="false"
+//             data-id="3"
+//           >
+//             <div>
+//               <div class="account__summary">
+//                 <div>
+//                   <p class="account__name">ネイマール</p>
+//                   <p class="account__team">パリサンジェルマン</p>
+//                 </div>
+//                 <div>
+//                   <button type="button" class="js-follow-btn followBtn">
+//                     フォローする
+//                   </button>
+//                 </div>
+//               </div>
+//               <p class="account__description">
+//                 ブラジル・サンパウロ州モジ・ダス・クルーゼス出身のサッカー選手。ブラジル代表。リーグ・アン・パリ・サンジェルマンFC所属。ポジションはフォワード
+//                 (wikipedia)
+//               </p>
+//             </div>
+//           </li>
+//         </ul>
+//       </div>
+//     </main>
+//     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+//     <script src="script.js"></script>
+//   </body>
+// </html> -->
+
+//script.js
+// $(function () {
+//   $(".js-account-item").each(function () {
+//     var $this = $(this);
+//     var $thisData = $this.data();
+//     if ($thisData.follow) {
+//       $this.find(".js-follow-btn").addClass("isFollow");
+//     }
+//   });
+
+//   $(".js-follow-btn").on("click", function () {
+//     var $this = $(this);
+//     var $accountItem = $this.parents(".js-account-item");
+//     var $thisData = $accountItem.data();
+
+//     if ($thisData.follow) {
+//       // ajax
+
+//       $this.removeClass("isFollow");
+//       $this.text("フォローする");
+//       $accountItem.data("follow", false);
+//     } else {
+//       // ajax
+
+//       $this.addClass("isFollow");
+//       $this.text("フォロー中");
+//       $accountItem.data("follow", true);
+//     }
+//   });
+// });
